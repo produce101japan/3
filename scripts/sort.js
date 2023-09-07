6b7dee80a09f849bd2e3d7f616203ac41a50eabc
produce101japan <54850202+produce101japan@users.noreply.github.com>
2023-09-07 21:04:18 +0900

    version

--- scripts/sort.js
+++ scripts/sort.js
@@ -1,7 +1,7 @@
 const MEMBER_FILE = {
   default: "trainee_info.csv"
 }
-const FILE_VERSION = "202309072056";
+const FILE_VERSION = "202309072104";
 const CURRENT_BORDER = 99;
 const CURRENT_RANK_COLUMN = 99;
 //for maker
